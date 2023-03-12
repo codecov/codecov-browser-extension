effe50f91e407656d97d4f10bb9067cc33cc49c9
Anukul Sangwan <anukulsangwan@icloud.com>
2023-03-12 19:37:29 +0400

    clean up deps

---
+++ src/popup/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+createRoot(
+  document.getElementById("app")!
+).render(<App />);
