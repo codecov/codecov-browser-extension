9cde8e2d379495c2881e69547a4277ec8af67377
Spencer Murray <spencer.murray@sentry.io>
2024-03-26 13:12:15 -0400

    Add regex examples

--- src/content/github/common/utils.ts
+++ src/content/github/common/utils.ts
@@ -1,6 +1,12 @@
 // Note that we're guaranteed to be on a github page due to manifest.json,
 // so these checks don't need to include that.
+
+// This one matches PR files pages. Something like:
+// /codecov/gazebo/pull/2435/files
 const prUrlRegex = /\/[^\/]+\/[^\/]+\/pull\/\d+\/files.*/
+
+// And this one matches file view pages - which look like:
+// /codecov/gazebo/blob/main/src/App.jsx
 const fileUrlRegex = /\/[^\/]+\/[^\/]+\/blob\/[^\/]+\/.*/
 
 export function isFileUrl(url: string): boolean {
