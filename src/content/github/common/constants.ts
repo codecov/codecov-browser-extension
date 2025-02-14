30ef2e1f739fad0a81890765b3e847c81cc3d43c
Spencer Murray <spencer.murray@sentry.io>
2025-02-14 15:15:54 -0500

    Use esnext, update typescript, remove color-alpha

--- src/content/github/common/constants.ts
+++ src/content/github/common/constants.ts
@@ -7,7 +7,10 @@ export const animationAttachmentId = `${animationName}-attachment`;
 export const seenClassName = "codecov-seen-mark";
 
 export const colors = {
+  redAlpha: "rgba(245,32,32,0.25)",
+  greenAlpha: "rgba(33,181,119,0.25)",
+  yellowAlpha: "rgba(244,176,27,0.25)",
   red: "rgb(245,32,32)",
   green: "rgb(33,181,119)",
-  yellow: "rgb(244,176,27)",
+  yellow: "rgba(244,176,27)",
 };
