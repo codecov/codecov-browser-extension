b2506015ab5ee41b67d566e8216aa26396d9f360
Spencer Murray <spencer.murray@sentry.io>
2025-07-08 13:46:06 -0400

    Add data paragraph and uninstall button to consent page

--- public/consent.js
+++ public/consent.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var button = document.getElementById("codecov-consent-accept");
-  button.addEventListener("click", async function () {
+  var consentButton = document.getElementById("codecov-consent-accept");
+  consentButton.addEventListener("click", async function () {
     const result = await browser.runtime.sendMessage({
       type: "set_consent",
       payload: true,
@@ -14,5 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
         "Something went wrong saving consent. Please report this at https://github.com/codecov/codecov-browser-extension/issues"
       );
     }
+  });
+
+  var uninstallButton = document.getElementById("codecov-uninstall");
+  uninstallButton.addEventListener("click", async function () {
+    await browser.management
+      .uninstallSelf()
+      .then(() => {
+        console.log("App uninstalled, closing tab");
+        close();
+      })
+      .catch(() => {
+        console.log(
+          "Uninstall confirmation declined or something else went wrong."
+        );
+        close();
+      });
   });
 });
